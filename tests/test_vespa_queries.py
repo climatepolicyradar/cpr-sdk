c990ed46dee7e4e3f42370246bb3e747664d6ddd
Mark <mark@climatepolicyradar.org>
2024-10-03 14:34:10 +0100

    Updating the vespa query tests.

--- tests/test_vespa_queries.py
+++ tests/test_vespa_queries.py
@@ -4,20 +4,21 @@ from vespa.io import VespaQueryResponse
 
 @pytest.mark.vespa
 @pytest.mark.parametrize(
-    "field,value",
+    "field,value,search_term",
     [
-        ("name", "sectors"),
-        ("id", "concept_137_137"),
-        ("model", "environment_model"),
-        ("timestamp", "2024-09-26T16:15:39.817896"),
+        ("name", "sectors", "contains"),
+        ("id", "concept_137_137", "contains"),
+        ("model", "environment_model", "contains"),
+        ("timestamp", "2024-09-26T16:15:39.817896", "contains"),
+        ("parent_concept_ids_flat", "Q1", "matches"),
     ],
 )
-def test_concepts(test_vespa, field, value) -> None:
+def test_concepts(test_vespa, field, search_term, value) -> None:
     """Test that we can retrieve concepts from a document."""
     response: VespaQueryResponse = test_vespa.client.query(
         yql="select * from document_passage "
         "where concepts contains "
-        f'sameElement({field} contains "{value}")'
+        f'sameElement({field} {search_term} "{value}")'
     )
 
     assert response.is_successful()
