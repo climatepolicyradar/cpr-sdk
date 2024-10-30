9af330f62dbafea7b6be287ae7c70a3d1340d185
Mark <mark@climatepolicyradar.org>
2024-10-30 10:39:01 +0000

    updating to allow param names.

--- src/cpr_sdk/ssm.py
+++ src/cpr_sdk/ssm.py
@@ -12,6 +12,9 @@ def get_aws_ssm_param(param_name: str, region_name: str = "eu-west-1") -> str:
 
 def get_vespa_search_adapter_from_aws_secrets(
     cert_dir: str,
+    vespa_instance_url_param_name: str = "VESPA_INSTANCE_URL",
+    vespa_public_cert_param_name: str = "VESPA_PUBLIC_CERT",
+    vespa_private_key_param_name: str = "VESPA_PRIVATE_KEY",
 ) -> VespaSearchAdapter:
     """
     Get a VespaSearchAdapter instance by retrieving secrets from AWS Secrets Manager.
@@ -19,9 +22,9 @@ def get_vespa_search_adapter_from_aws_secrets(
     We then save the secrets to local files in the cert_dir directory and instantiate
     the VespaSearchAdapter.
     """
-    vespa_instance_url = get_aws_ssm_param("VESPA_INSTANCE_URL")
-    vespa_public_cert = get_aws_ssm_param("VESPA_PUBLIC_CERT")
-    vespa_private_key = get_aws_ssm_param("VESPA_PRIVATE_KEY")
+    vespa_instance_url = get_aws_ssm_param(vespa_instance_url_param_name)
+    vespa_public_cert = get_aws_ssm_param(vespa_public_cert_param_name)
+    vespa_private_key = get_aws_ssm_param(vespa_private_key_param_name)
 
     with open(f"{cert_dir}/cert.pem", "w") as f:
         f.write(vespa_public_cert)
