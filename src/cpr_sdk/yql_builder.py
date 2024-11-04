87fd77b546cb70988142af7ee69a76d8c950a295
Kalyan Dutia <kalyan.dutia@gmail.com>
2024-11-04 11:11:13 +0000

    swap weakAnd to userInput in YQL

--- src/cpr_sdk/yql_builder.py
+++ src/cpr_sdk/yql_builder.py
@@ -58,26 +58,16 @@ class YQLBuilder:
         elif self.sensitive:
             return """
                 (
-                    {"targetHits": 1000} weakAnd(
-                        family_name contains(@query_string),
-                        family_description contains(@query_string),
-                        text_block contains(@query_string)
-                    )
+                    userInput(@query_string)
                 )
             """
         else:
             return """
                 (
                     (
-                    {"targetHits": 1000} weakAnd(
-                        family_name contains(@query_string),
-                        family_description contains(@query_string),
-                        text_block contains(@query_string)
-                    )
-                    ) or (
-                        [{"targetNumHits": 1000}]
-                        nearestNeighbor(family_description_embedding,query_embedding)
-                    ) or (
+                        userInput(@query_string)
+                    ) 
+                    or (
                         [{"targetNumHits": 1000}]
                         nearestNeighbor(text_embedding,query_embedding)
                     )
