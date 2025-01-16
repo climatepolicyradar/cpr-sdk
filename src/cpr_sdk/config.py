5fb9a97d7a5d1a624fb7cca4674314d6eb3d1cf7
Kalyan Dutia <kalyan.dutia@gmail.com>
2025-01-16 17:11:11 +0000

    remove use of find_dotenv

--- src/cpr_sdk/config.py
+++ src/cpr_sdk/config.py
@@ -1,9 +1,9 @@
 import os
 from typing import Optional
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 VESPA_URL: Optional[str] = os.environ.get("VESPA_URL")
 if VESPA_URL is not None:
