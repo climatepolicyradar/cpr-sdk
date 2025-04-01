a87cb11382eecdf0de0a10061ec8077f9229c09d
olaughter <fred@climatepolicyradar.org>
2025-04-01 15:35:08 +0100

    Adding slack hook to sdk

---
+++ tests/orchestration/test_hooks.py
@@ -0,0 +1,21 @@
+from cpr_sdk.orchestration.hooks import SlackNotify
+
+
+def test_message(mock_prefect_slack_webhook, mock_flow, mock_flow_run):
+    SlackNotify.message(mock_flow, mock_flow_run, mock_flow_run.state)
+    mock_SlackWebhook, mock_prefect_slack_block = mock_prefect_slack_webhook
+
+    # `.load`
+    mock_SlackWebhook.load.assert_called_once_with(
+        "slack-webhook-platform-prefect-mvp-sandbox"
+    )
+
+    # `.notify`
+    mock_prefect_slack_block.notify.assert_called_once()
+    kwargs = mock_prefect_slack_block.notify.call_args.kwargs
+    message = kwargs.get("body", "")
+    assert message == (
+        "Flow run TestFlow/TestFlowRun observed in state `Completed` at "
+        "2025-01-28T12:00:00+00:00. For environment: sandbox. Flow run URL: "
+        "None/flow-runs/flow-run/test-flow-run-id. State message: message"
+    )
