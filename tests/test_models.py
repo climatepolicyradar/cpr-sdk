161fca900ccc734e1b248b5a8df0b71516d92f75
Jesse Claven <jesse.claven@me.com>
2025-09-15 16:29:13 +0100

    feat: Add concept model
    This includes fixtures.

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,7 @@ import json
 import pandas as pd
 import pytest
 from datasets import Dataset as HuggingFaceDataset
-from pydantic import ValidationError
+from pydantic import AnyHttpUrl, ValidationError
 
 from cpr_sdk.models import (
     BaseDocument,
@@ -20,7 +20,7 @@ from cpr_sdk.models import (
     Span,
     TextBlock,
 )
-from cpr_sdk.models.search import Passage, MetadataFilter
+from cpr_sdk.models.search import Concept, JsonDict, Passage, MetadataFilter, WikibaseId
 
 
 @pytest.fixture
@@ -659,3 +659,56 @@ def test_vespa_concept_json_conversion() -> None:
             timestamp=datetime.now(),
         ).model_dump(mode="json")
     )
+
+
+def test_concept_from_response_hit():
+    response_hit: JsonDict = JsonDict(
+        {
+            "id": "id:doc_search:concept::w2m7ymf7",
+            "relevance": 0.0,
+            "source": "family-document-passage",
+            "fields": {
+                "sddocname": "concept",
+                "documentid": "id:doc_search:concept::w2m7ymf7",
+                "id": "w2m7ymf7",
+                "wikibase_id": "Q290",
+                "wikibase_url": "https://wikibase.example.org/wiki/Item:Q290",
+                "preferred_label": "pollution",
+                "subconcept_of": ["hzaw3hdg"],
+                "recursive_has_subconcept": [],
+            },
+        }
+    )
+
+    assert Concept(
+        id="w2m7ymf7",
+        wikibase_id=WikibaseId(numeric=290),
+        wikibase_url=AnyHttpUrl("https://wikibase.example.org/wiki/Item:Q290"),
+        preferred_label="pollution",
+        description=None,
+        definition=None,
+        alternative_labels=[],
+        negative_labels=[],
+        subconcept_of=["hzaw3hdg"],
+        has_subconcept=[],
+        related_concepts=[],
+        recursive_concept_of=None,
+        recursive_has_subconcept=[],
+        response_raw=JsonDict(
+            {
+                "id": "id:doc_search:concept::w2m7ymf7",
+                "relevance": 0.0,
+                "source": "family-document-passage",
+                "fields": {
+                    "sddocname": "concept",
+                    "documentid": "id:doc_search:concept::w2m7ymf7",
+                    "id": "w2m7ymf7",
+                    "wikibase_id": "Q290",
+                    "wikibase_url": "https://wikibase.example.org/wiki/Item:Q290",
+                    "preferred_label": "pollution",
+                    "subconcept_of": ["hzaw3hdg"],
+                    "recursive_has_subconcept": [],
+                },
+            }
+        ),
+    ) == Concept.from_vespa_response(response_hit)
