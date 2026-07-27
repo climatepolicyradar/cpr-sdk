5e4d7e91b6a45961c394f5c22763b8aebadfe269
Mark <mark@climatepolicyradar.org>
2026-07-27 10:56:29 +0100

    feat: Remove feed commands for now removed indexes.

--- vespa.mk
+++ vespa.mk
@@ -30,9 +30,6 @@ vespa_deploy_app:
 .ONESHELL:
 vespa_load_data:
 	vespa feed --target local --wait 60 tests/local_vespa/test_documents/*.json
-	vespa feed --target local --wait 60 tests/local_vespa/test_documents/concept/*.json
-	vespa feed --target local --wait 60 tests/local_vespa/test_documents/classifiers_profile/*.json
-	vespa feed --target local --wait 60 tests/local_vespa/test_documents/classifiers_profiles/*.json
 
 # Deletes all test data from local Vespa instance
 .ONESHELL:
